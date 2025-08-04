513381364814f2dec296d50d17c3ac266dd980fb
Dannyleo44 <d399197@gmail.com>
2025-08-04 23:32:17 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: '𝐉ᴜɴᴇ 𝐌ᴅ',
   author: 'supreme',
   botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
-  botOwner: 'Supreme', // Your name
-  ownerNumber: '254798570132', //Set your number here without + symbol.
+  botOwner: 'Dannyleo', // Your name
+  ownerNumber: '+2349157809901', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
